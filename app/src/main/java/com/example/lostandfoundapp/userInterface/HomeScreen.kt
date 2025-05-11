227e058c90ff0a01dd9f1fd0dcf5a07c3c1f56ee
Aliisa Rokala <aliisarokala@protonmail.com>
2025-05-11 17:21:08 +0300

    updated gitingore

--- app/src/main/java/com/example/lostandfoundapp/userInterface/HomeScreen.kt
+++ app/src/main/java/com/example/lostandfoundapp/userInterface/HomeScreen.kt
@@ -1,12 +1,10 @@
 package com.example.lostandfoundapp.userInterface
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
